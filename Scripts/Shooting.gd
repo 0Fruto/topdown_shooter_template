3b10b12a71f8efbfc6902c86d29ec818fdd6ebbc
fruto <maxmart999@icloud.com>
2020-05-05 12:57:48 +0300

    Reloading
    -New reloading animations
    
    -New font
    
    -Propreate colliders

--- Scripts/Shooting.gd
+++ Scripts/Shooting.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 var bullet
 var loadBullet
-#export (PackedScene) var pause       does not working
 export var cdGoal = 0.2
 var cd = 0
 var bullets
+var reloading = false
+export var reloadingDuration = 100
+var reloadingCounter = reloadingDuration
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -15,33 +17,55 @@ func _ready():
 
 func ReloadWeapon():
 	if $Player.magazineCount >= 1:
-		bullets = $Player.fullMagazine
-		$Player.magazineCount -= 1
+		reloading = true
+		$Player/Character/Gun.play("Reloading")
+		$Player/Character.play("Reloading")
+
+func Shooting():
+	$Player/Character/Gun.play("Shooting")
+	$Player/Character.play("Shooting")
+	$Cursor.play("Shooting")
+	$"Player/Player Shoot col".disabled = false
+	$"Player/Player Idle col".disabled = true
+	bullet = loadBullet.instance()
+	add_child(bullet)
+	cd = 0
+	bullets -= 1
+	
+func Prepare():
+	$Player/Character/Gun.show()
+	$Player/Character.play("Prepare")
+	$Player/Character/Gun.play("Prepare")
+	$Cursor.play("Idle")
+	$"Player/Player Shoot col".disabled = false
+	$"Player/Player Idle col".disabled = true
+	if bullets < 1:
+		ReloadWeapon()
+
+func RemoveWeapon():
+	$Cursor.play("Idle")
+	$Player/Character/Gun.hide()
+	$Player/Character.play("Idle")
+	$"Player/Player Shoot col".disabled = true
+	$"Player/Player Idle col".disabled = false
 
 func _process(delta):
-	$"Player/Main camera/Label".text = (str(bullets) + "/" + str($Player.magazineCount))
+	$"Player/Main camera/Bullets".text = (str(bullets) + "/" + str($Player.magazineCount))
 	cd += delta
-#	if Input.is_action_just_pressed("ui_cancel"):     does not working
-#		var inst = pause.instance()     does not working
-#		add_child(inst)     does not working
-	if Input.is_action_pressed("ui_select"):
-		$Player/Character/Gun.show()
+	if Input.is_action_pressed("ui_select") and not reloading:
 		if cd >= cdGoal and bullets > 0:
-			$Player/Character/Gun.play("Shooting")
-			$Player/Character.play("Shooting")
-			$Cursor.play("Shooting")
-			bullet = loadBullet.instance()
-			add_child(bullet)
-			cd = 0
-			bullets -= 1
+			Shooting()
 		else:
-			$Player/Character.play("Prepare")
-			$Player/Character/Gun.play("Prepare")
-			$Cursor.play("Idle")
-			if bullets < 1:
-				ReloadWeapon()
+			Prepare()
 	else:
-		$Cursor.play("Idle")
-		$Player/Character/Gun.hide()
-		$Player/Character.play("Idle")
-		
+		if not reloading:
+			RemoveWeapon()
+	
+	
+	if reloading:
+		reloadingCounter -= 1
+		if reloadingCounter <= 0:
+			bullets = $Player.fullMagazine
+			$Player.magazineCount -= 1
+			reloading = false
+			reloadingCounter = reloadingDuration
